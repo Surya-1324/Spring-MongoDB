c04533df193814c02cc2d05f190980e4cb2805b1
suryasanjay321 <116817102+suryasanjay321@users.noreply.github.com>
2024-05-04 21:54:15 +0530

    initial push

---
+++ MongoDB/src/test/java/com/example/MongoDB/MongoDbApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootApplication
+class MongoDbApplicationTests {
+	public static void main(String args[]){
+		SpringApplication.run(MongoDbApplicationTests.class,args);
+	}
+
+}
